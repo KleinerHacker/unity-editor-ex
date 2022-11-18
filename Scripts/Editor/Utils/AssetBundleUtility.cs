78504b7e7e42dcc296b085a067854651343f47e6
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2022-11-18 22:18:03 +0100

    Add useful utility extensions

---
+++ Scripts/Editor/Utils/AssetBundleUtility.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityEditorEx.Editor.editor_ex.Scripts.Editor.Utils
+{
+    public static class AssetBundleUtility
+    {
+        public static void CreateAssetBundle(string name)
+        {
+            var assetImporter = AssetImporter.GetAtPath("Assets");
+            var oldName = assetImporter.assetBundleName;
+
+            assetImporter.assetBundleName = name;
+            assetImporter.SaveAndReimport();
+
+            assetImporter.assetBundleName = oldName;
+            assetImporter.SaveAndReimport();
+        }
+
+        public static bool HasBundle(Object o)
+        {
+            var assetPath = AssetDatabase.GetAssetPath(o);
+            var assetImporter = AssetImporter.GetAtPath(assetPath);
+
+            return !string.IsNullOrEmpty(assetImporter.assetBundleName);
+        }
+    }
+}
---
+++ Scripts/Editor/Windows/InputWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityEditorEx.Editor.editor_ex.Scripts.Editor.Windows
+{
+    internal sealed class InputWindow : EditorWindow
+    {
+        #region Properties
+
+        public string Message { get; set; }
+        public string Text { get; set; }
+        public string OkButtonText { get; set; }
+        public string CancelButtonText { get; set; }
+        public bool? ResultState { get; private set; }
+
+        #endregion
+
+        private void OnEnable()
+        {
+            minSize = new Vector2(300f, 75f);
+            maxSize = minSize;
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label(Message);
+            Text = GUILayout.TextField(Text);
+            
+            GUILayout.BeginHorizontal();
+            {
+                if (GUILayout.Button(OkButtonText))
+                {
+                    ResultState = true;
+                    Close();
+                }
+
+                if (GUILayout.Button(CancelButtonText))
+                {
+                    ResultState = false;
+                    Close();
+                }
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
