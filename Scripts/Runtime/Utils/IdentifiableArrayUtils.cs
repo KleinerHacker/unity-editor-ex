7355d9021b3ce5d1759c7e9d1dd8ca54c4e8d71b
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2024-11-27 19:34:44 +0100

    fix wrong update array function

--- Scripts/Runtime/Utils/IdentifiableArrayUtils.cs
+++ Scripts/Runtime/Utils/IdentifiableArrayUtils.cs
@@ -7,37 +7,42 @@ namespace UnityEditorEx.Runtime.Projects.unity_editor_ex.Scripts.Runtime.Utils
 {
     public static class IdentifiableArrayUtils
     {
-        public static T[] CreateIdentifiableArray<T,TI>(params TI[] excludes) where T : IIdentifiableObject<TI>
+        public static T[] CreateIdentifiableArray<T,TI>(params TI[] excludes) where T : IIdentifiableObject<TI> where TI : Enum
         {
-            var sceneStates = Enum.GetValues(typeof(TI)).Cast<TI>().Distinct().ToArray();
+            var values = Enum.GetValues(typeof(TI)).Cast<TI>().Distinct().ToArray();
             var list = new List<T>();
 
-            foreach (var state in sceneStates)
+            foreach (var value in values)
             {
-                if (excludes.Contains(state))
+                if (excludes.Contains(value))
                     continue;
                 
-                list.Add((T) typeof(T).GetConstructor(new []{typeof(TI)}).Invoke(new object[] {state}));
+                list.Add((T) typeof(T).GetConstructor(new []{typeof(TI)}).Invoke(new object[] {value}));
             }
 
             return list.ToArray();
         }
         
-        public static T[] UpdateIdentifiableArray<T,TI>(params T[] existing) where T : IIdentifiableObject<TI>
+        public static T[] UpdateIdentifiableArray<T,TI>(params T[] existing) where T : IIdentifiableObject<TI> where TI : Enum
         {
-            var sceneStates = Enum.GetValues(typeof(TI)).Cast<TI>().Distinct().ToArray();
+            var values = Enum.GetValues(typeof(TI)).Cast<TI>().Distinct().ToArray();
+            return UpdateIdentifiableArray<T, TI>(existing, values);
+        }
+
+        public static T[] UpdateIdentifiableArray<T, TI>(T[] existing, params TI[] values) where T : IIdentifiableObject<TI>
+        {
             var list = new List<T>();
 
-            foreach (var state in sceneStates)
+            foreach (var value in values)
             {
-                var element = existing.FirstOrDefault(x => Equals(x.Identifier, state));
+                var element = existing.FirstOrDefault(x => Equals(x.Identifier, value));
                 if (element != null)
                 {
                     list.Add(element);
                 }
                 else
                 {
-                    list.Add((T) typeof(T).GetConstructor(new[] {typeof(TI)}).Invoke(new object[] {state}));
+                    list.Add((T) typeof(T).GetConstructor(new[] {typeof(TI)}).Invoke(new object[] {value}));
                 }
             }
 
